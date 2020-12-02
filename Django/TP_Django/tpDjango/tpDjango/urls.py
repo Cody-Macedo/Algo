ff2e130d3f3c35820d255411043e461466732c97
Cody-Macedo <macedo.cody@gmail.com>
2020-12-02 10:58:40 +0100

    first view init in ToDoList

--- Django/TP_Django/tpDjango/tpDjango/urls.py
+++ Django/TP_Django/tpDjango/tpDjango/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 import debug_toolbar
@@ -21,4 +22,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todolist/', include('toDoList.urls')),
 ]
