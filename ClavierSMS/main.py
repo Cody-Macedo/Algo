127e7b4b5201591d5a1b57695679cbebc3272169
Cody-Macedo <macedo.cody@gmail.com>
2021-11-05 12:04:43 +0100

    update computeNumbers

--- ClavierSMS/main.py
+++ ClavierSMS/main.py
@@ -1,4 +1,46 @@
-def add(nb):
-    return nb + 2
+from typing import Tuple
+
+pad = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
 
 
+def getFrenchList() -> Tuple:
+    with open('liste_francais.txt') as file:
+        lines = file.read()
+    words_tuple = tuple(word for word in lines.split('\n'))
+    return words_tuple
+
+
+def computeNumbers(numbers: str):
+    all_words = getFrenchList()
+
+    words_filtered = []
+    for word_filter in all_words:
+        if len(word_filter) == len(numbers):
+            # print(word_filter)
+            words_filtered.append(word_filter)
+
+    words_result = []
+    for word in words_filtered:
+        word_match = True
+        for index, letter in enumerate(word):
+            # print("index : ", index, "/ letter :", letter)
+            if letter not in pad[numbers[index]]:
+                word_match = False
+                break
+        if word_match:
+            words_result.append(word)
+    return words_result
+
+
+print(computeNumbers("2665687"))
+print(computeNumbers("72588"))
+print(computeNumbers("2662273437"))
+
+# def listLetters(number):
+#     list_letters = []
+#
+#     for i in str(number):
+#         if 1 < int(i) < 10:
+#             list_letters.append(pad[int(i)])
+#
+#     return list_letters
--- ClavierSMS/tdd.py
+++ ClavierSMS/tdd.py
@@ -1,6 +1,13 @@
 import main
 
 
-def test_add_1():
-    result = main.add(2)
-    assert result == 4
+def test_compute_1():
+    assert main.computeNumbers("2665687") == ['bonjour']
+
+
+def test_compute_2():
+    assert main.computeNumbers("72588") == ['salut']
+
+
+def test_compute_3():
+    assert main.computeNumbers("2662273437") == ['bombardier']
